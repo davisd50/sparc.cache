a7cbf5a5990c7b8e51d0daa8faabf08ddf2f5cb0
David Davis <davisd50@gmail.com>
2016-07-01 16:22:19 -0400

    add Splunk KV Collection ICacheArea

---
+++ sparc/cache/splunk/area.py
@@ -0,0 +1,157 @@
+import json
+import requests
+import xml.etree.ElementTree as ET
+from zope.component.factory import Factory
+from zope.event import notify
+from zope.interface import implements
+from sparc.cache.events import CacheObjectCreatedEvent, CacheObjectModifiedEvent
+from sparc.cache import ICacheArea
+from sparc.db.splunk.kvstore import current_kv_names
+
+from sparc.logging import logging
+logger = logging.getLogger(__name__)
+
+class CacheAreaForSplunkKV(object):
+    """An area where cached information can be stored persistently."""
+    implements(ICacheArea)
+
+    def __init__(self, mapper, schema, sci, kv_id):
+        """Object initializer
+
+        Args:
+            mapper: Object providing sparc.cache.ICachedItemMapper that will
+                    convert ICachableItem instances into ICachedItem instances.
+            schema: Object providing sparc.db.splunk.ISplunkKVCollectionSchema.
+            sci: sparc.db.splunk.ISplunkConnectionInfo instance to provide
+                 connection information for Splunk indexing server
+            kv_id: Object providing sparc.db.splunk.ISPlunkKVCollectionIdentifier
+        """
+        self.mapper = mapper
+        self.schema = schema
+        self.sci = sci
+        self.kv_id = kv_id
+        self.collname = kv_id.collection
+        self.appname = kv_id.application
+        self.username = kv_id.username
+        self.url = "".join(['https://',sci['host'],':',sci['port'],
+                                    '/servicesNS/',self.username,'/',
+                                                        self.appname,'/'])
+        self.auth = (self.sci['username'], self.sci['password'], )
+
+    def current_kv_names(self):
+        """Return set of string names of current available Splunk KV collections"""
+        return current_kv_names(self.sci, self.username, self.appname)
+    
+    def _data(self, CachedItem):
+        data = {k:getattr(CachedItem, k) for k in self.mapper.mapper}
+        data['_key'] = CachedItem.getId()
+        return data
+    
+    def _add(self, CachedItem):
+        r = requests.post(self.url+"storage/collections/data/"+self.collname,
+                        auth=self.auth,
+                        headers = {'Content-Type': 'application/json'},
+                        data=json.dumps(self._data(CachedItem)), 
+                        verify=False)
+        r.raise_for_status()
+    
+    def _update(self, CachedItem):
+        r = requests.post(self.url+"storage/collections/data/"+self.collname+'/'+CachedItem.getId(),
+                        auth=self.auth,
+                        headers = {'Content-Type': 'application/json'},
+                        data=json.dumps(self._data(CachedItem)), 
+                        verify=False)
+        r.raise_for_status()
+
+    #ICacheArea
+    def get(self, CachableItem):
+        """Returns current ICachedItem for ICachableItem or None if not cached"""
+        cached_item = self.mapper.get(CachableItem)
+        r = requests.get(self.url+"storage/collections/data/"+\
+                                        self.collname+'/'+cached_item.getId(),
+                            auth=self.auth,
+                            data={'output_mode': 'json'},
+                            verify=False)
+        if r.ok:
+            # we need to update the object with the values found in the cache area
+            data = r.json()
+            for name in self.mapper.mapper:
+                setattr(cached_item, name, data[name])
+            return cached_item
+        return None
+        
+
+    def isDirty(self, CachableItem):
+        """True if cached information requires update for ICachableItem"""
+        _cachedItem = self.get(CachableItem)
+        if not _cachedItem:
+            return True
+        _newCacheItem = self.mapper.get(CachableItem)
+        return False if _cachedItem == _newCacheItem else True
+
+    def cache(self, CachableItem):
+        """Updates caches area with latest item information returning
+           ICachedItem if cache updates were required.
+
+           Issues ICacheObjectCreatedEvent, and ICacheObjectModifiedEvent for
+           ICacheArea/ICachableItem combo.
+        """
+        _cachedItem = self.get(CachableItem)
+        if not _cachedItem:
+            _cachedItem = self.mapper.get(CachableItem)
+            self._add(_cachedItem)
+            logger.debug("new cachable item added to Splunk KV cache area {id: %s, type: %s}", str(_cachedItem.getId()), str(_cachedItem.__class__))
+            notify(CacheObjectCreatedEvent(_cachedItem, self))
+            return _cachedItem
+        else:
+            _newCacheItem = self.mapper.get(CachableItem)
+            if _cachedItem != _newCacheItem:
+                logger.debug("Cachable item modified in Splunk KV cache area {id: %s, type: %s}", str(_newCacheItem.getId()), str(_newCacheItem.__class__))
+                self._update(_newCacheItem)
+                notify(CacheObjectModifiedEvent(_newCacheItem, self))
+                return _newCacheItem
+        return None
+
+    def import_source(self, CachableSource):
+        """Updates cache area and returns number of items updated with all
+           available entries in ICachableSource
+        """
+        _count = 0
+        for item in CachableSource.items():
+            if self.cache(item):
+                _count += 1
+        return _count
+
+    def reset(self):
+        """Deletes all entries in the cache area"""
+        if self.collname not in self.current_kv_names():
+            return # nothing to do
+        # we'll simply delete the entire collection and then re-create it.
+        r = requests.delete(self.url+"storage/collections/data/"+self.collname,
+                        auth=self.auth,
+                        verify=False)
+        r.raise_for_status()
+        self.initialize()
+        
+    def initialize(self):
+        """Instantiates the cache area to be ready for updates"""
+        if self.collname not in self.current_kv_names():
+            r = requests.post(self.url+"storage/collections/config",
+                            auth=self.auth,
+                            headers = {'content-type': 'application/json'},
+                            data={'name': self.collname},
+                            verify=False)
+            r.raise_for_status()
+        # initialize schema
+        re = requests.post(self.url+"storage/collections/config/"+self.collname,
+                            auth=self.auth,
+                            headers = {'content-type': 'application/json'},
+                            data=self.schema,
+                            verify=False)
+        re.raise_for_status()
+        logger.info("initialized Splunk Key Value Collection %s with schema %s"\
+                        % (self.collname, str(self.schema)))
+        if self.collname not in self.current_kv_names():
+            raise EnvironmentError('expected %s in list of kv collections %s' % (self.collname, str(self.current_kv_names())))
+
+cacheAreaForSplunkKVFactory = Factory(CacheAreaForSplunkKV)
---
+++ sparc/cache/splunk/tests/test_area.py
@@ -0,0 +1,168 @@
+import os
+import unittest
+import zope.testrunner
+from zope import component
+from sparc.testing.fixture import test_suite_mixin
+from sparc.db.splunk.testing import SPARC_DB_SPLUNK_INTEGRATION_LAYER
+
+import requests
+from zope.component.eventtesting import getEvents
+from zope.interface import alsoProvides
+from sparc.cache import ICachableItem
+from sparc.cache.events import ICacheObjectCreatedEvent
+from sparc.cache.events import ICacheObjectModifiedEvent
+from sparc.cache.splunk.area import CacheAreaForSplunkKV
+from sparc.db.splunk import ISplunkKVCollectionSchema
+from sparc.db.splunk.tests.test_kvstore import ITestSchema
+
+kv_names = {}
+kv_names['test_collection'] = {}
+kv_names['test_collection']['field.id'] = "string"
+kv_names['test_collection']['field.name'] = "string"
+SPARC_DB_SPLUNK_INTEGRATION_LAYER.kv_names.update(kv_names)
+
+class SparcCacheSplunkAreaTestCase(unittest.TestCase):
+    level = 2
+    layer = SPARC_DB_SPLUNK_INTEGRATION_LAYER
+    sm = component.getSiteManager()
+    
+    def cachable_item(self, id=None, name=None):
+        cachable_item = type('CachableItem', (object,), {'attributes':{'id':id,'name':name}})
+        alsoProvides(cachable_item, ICachableItem)
+        return cachable_item
+    
+    def kv_id(self, collection):
+        kv_id = component.createObject(u'sparc.db.splunk.kv_collection_identifier')
+        kv_id.collection = collection
+        kv_id.application = self.layer.kv_appname
+        kv_id.username = self.layer.kv_username
+        return kv_id
+
+    def setUp(self):
+        self.tearDown() # destroy any left over kv collections from these tests
+        self.mapper = component.createObject(u'sparc.cache.simple_item_mapper', 
+                                        key='id',
+                                        CacheableItem=self.cachable_item())
+        schema = component.createObject(u'sparc.db.splunk.kv_collection_schema')
+        schema.update(kv_names['test_collection'])
+        self.cache_area = CacheAreaForSplunkKV(self.mapper, 
+                                schema, self.layer.sci, 
+                                self.kv_id(u"test_collection"))
+    
+    def tearDown(self):
+        collections = ['test_init']
+        for name in [n for n in  collections\
+                                if n in self.layer.get_current_kv_names()]:
+            requests.delete(self.layer.url+"storage/collections/config/"+name,
+                                auth=self.layer.auth,
+                                verify=False)
+        names = self.layer.get_current_kv_names()
+        for name in collections:
+            if name in names:
+                raise EnvironmentError('unexpectedly found %s in kv collections %s' % (name, str(self.kv_names.keys())))
+
+    def test_initialize(self):
+        from zope import schema as zschema
+        cachable_item = type('CachableItem', (object,),
+                             {'attributes':{},'key':'int'})
+        cachable_item.attributes = {n:None for n in ITestSchema}
+        alsoProvides(cachable_item, ICachableItem)
+        mapper = component.createObject(u'sparc.cache.simple_item_mapper', 
+                                        key='int',
+                                        CacheableItem=cachable_item)#,
+                                        #filter=lambda x,y: str(y))
+        schema = ISplunkKVCollectionSchema(ITestSchema)
+        ca = CacheAreaForSplunkKV(mapper, schema, self.layer.sci, 
+                                  self.kv_id(u"test_init"))
+        self.assertNotIn('test_init', self.layer.get_current_kv_names())
+        ca.initialize()
+        self.assertIn('test_init', self.layer.get_current_kv_names())
+        
+        kv_id = self.layer.get_kv_id(u'test_init')
+        schema_test = component.getMultiAdapter((self.layer.sci, kv_id,), 
+                                                ISplunkKVCollectionSchema)
+        self.assertEquals(schema, schema_test)
+        
+        # add a new field and init again...see if it creates the new field
+        ITestSchema.new = zschema.TextLine(title=u'new')
+        schema = ISplunkKVCollectionSchema(ITestSchema)
+        ca = CacheAreaForSplunkKV(mapper, schema, self.layer.sci, 
+                                  self.kv_id(u"test_init"))
+        ca.initialize()
+        schema_test = component.getMultiAdapter((self.layer.sci, kv_id,), 
+                                                ISplunkKVCollectionSchema)
+        self.assertEquals(schema, schema_test)
+    
+    def test_get_cache(self):
+        cachable_item = self.cachable_item('123','a name')
+        self.assertIsNone(self.cache_area.get(cachable_item))
+        self.assertEquals(getEvents(ICacheObjectCreatedEvent), [])
+        self.assertEquals(getEvents(ICacheObjectModifiedEvent), [])
+        
+        cached_item = self.cache_area.cache(cachable_item)
+        test_cached_item = self.cache_area.get(cachable_item)
+        self.assertEquals(cached_item, test_cached_item)
+        self.assertEquals(len(getEvents(ICacheObjectCreatedEvent)), 1)
+        self.assertEquals(getEvents(ICacheObjectModifiedEvent), [])
+        
+        self.assertIsNone(self.cache_area.cache(cachable_item))
+        cachable_item.attributes['name'] = 'another name'
+
+        test_cached_item = self.cache_area.cache(cachable_item)
+        self.assertIsNotNone(test_cached_item)
+        self.assertNotEquals(cached_item, test_cached_item)
+        self.assertEquals(cached_item.getId(), test_cached_item.getId())
+        self.assertEquals(len(getEvents(ICacheObjectCreatedEvent)), 1)
+        self.assertEquals(len(getEvents(ICacheObjectModifiedEvent)), 1)
+    
+    def test_isDirty(self):
+        cachable_item = self.cachable_item('abcd','a name')
+        self.assertTrue(self.cache_area.isDirty(cachable_item))
+        self.cache_area.cache(cachable_item)
+        self.assertFalse(self.cache_area.isDirty(cachable_item))
+        cachable_item = self.cachable_item('abcd','another name')
+        self.assertTrue(self.cache_area.isDirty(cachable_item))
+        self.cache_area.cache(cachable_item)
+        self.assertFalse(self.cache_area.isDirty(cachable_item))
+    
+    def test_reset(self):
+        cachable_item = self.cachable_item('abcd','a name')
+        self.cache_area.cache(cachable_item)
+        self.cache_area.reset()
+        self.assertTrue(self.cache_area.isDirty(cachable_item))
+        self.cache_area.cache(cachable_item)
+        self.assertFalse(self.cache_area.isDirty(cachable_item))
+    
+    def test_import_source(self):
+        ci1 = self.cachable_item('123','name 1')
+        ci2 = self.cachable_item('abc','name 2')
+        cs = type('TestCacheSource', (object, ),{'items': None})
+        def items(self):
+            for ci in [ci1, ci2]:
+                yield ci
+        cs.items = items.__get__(self, cs.__class__) # creates a bound method for TestCacheSource type
+        count = self.cache_area.import_source(cs)
+        self.assertEquals(count, 2)
+
+# this will insure the doc test clean-up will happen for the created KV collections
+kv_names['type1'] = {}
+kv_names['type2'] = {}
+SPARC_DB_SPLUNK_INTEGRATION_LAYER.kv_names.update(kv_names)
+class test_suite(test_suite_mixin):
+    package = 'sparc.cache.splunk'
+    module = 'area'
+    level = 2
+    layer = SPARC_DB_SPLUNK_INTEGRATION_LAYER
+    
+    def __new__(cls):
+        suite = super(test_suite, cls).__new__(cls)
+        suite.addTest(unittest.makeSuite(SparcCacheSplunkAreaTestCase))
+        return suite
+
+
+if __name__ == '__main__':
+    zope.testrunner.run([
+                         '--path', os.path.dirname(__file__),
+                         '--tests-pattern', os.path.splitext(
+                                                os.path.basename(__file__))[0]
+                         ])
